7553b9f91b4b7e7d445bde15123bf4f3c70e51c8
Lawrence Ibarria <lawrence@verdantrobotics.com>
2019-05-18 20:57:59 +0000

    First steps on serialization, serializing class

---
+++ include/cbuf_stream.h
@@ -0,0 +1,45 @@
+#pragma once
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+class cbuf_ostream
+{
+  std::map<uint64_t, std::string> dictionary;
+  int stream = -1;
+
+public:
+  cbuf_ostream() {}
+  ~cbuf_ostream() { close(); }
+
+  void close()
+  {
+    if (stream != -1) {
+      close(stream);
+    }
+    stream = -1;
+  }
+
+  bool open_file(const char *fname)
+  {
+    stream = open(fname, O_WRONLY | O_APPEND, O_CREAT);
+    return stream != -1;
+  }
+
+  bool open_socket(const char *ip, int port)
+  {
+
+  }
+
+  template <class cbuf_struct>
+  bool serialize(cbuf_struct * member)
+  {
+    // check if we have serialized this type before or not.
+    // If not, serialize its metadata
+
+    // Serialize the data of the member itself
+    char* ptr = member->encode();
+    write(stream, ptr, member->encode_size());
+    member->free_encode(ptr);
+  }
+};
