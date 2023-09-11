8e26dfa043530dedbd2d10c0ca174eb405e62257
John Hurliman <jhurliman@jhurliman.org>
2023-09-11 15:49:58 -0700

    ?

--- src/CPrinter.cpp
+++ src/CPrinter.cpp
@@ -95,13 +95,12 @@ void CPrinter::print(ast_const* cst) {
 
 void CPrinter::print(ast_namespace* sp) {
   // Emit GCC and clang pragmas to disable -Wpacked
-  buffer->print("#if defined(__GNUC__)\n");
+  buffer->print("#if defined(__clang__)\n");
+  buffer->print("#pragma clang diagnostic push\n");
+  buffer->print("#pragma clang diagnostic ignored \"-Wunaligned-access\"\n");
+  buffer->print("#elif defined(__GNUC__)\n");
   buffer->print("#pragma GCC diagnostic push\n");
   buffer->print("#pragma GCC diagnostic ignored \"-Wpacked\"\n");
-  buffer->print("#pragma GCC diagnostic ignored \"-Wunaligned-access\"\n");
-  buffer->print("#elif defined(__clang__)\n");
-  buffer->print("#pragma clang diagnostic push\n");
-  buffer->print("#pragma clang diagnostic ignored \"-Wpacked\"\n");
   buffer->print("#endif\n\n");
 
   buffer->print("namespace %s {\n", sp->name);
@@ -127,10 +126,10 @@ void CPrinter::print(ast_namespace* sp) {
   buffer->print("};\n\n");
 
   // Emit GCC and clang pragmas to re-enable -Wpacked
-  buffer->print("#if defined(__GNUC__)\n");
-  buffer->print("#pragma GCC diagnostic pop\n");
-  buffer->print("#elif defined(__clang__)\n");
+  buffer->print("#if defined(__clang__)\n");
   buffer->print("#pragma clang diagnostic pop\n");
+  buffer->print("#elif defined(__GNUC__)\n");
+  buffer->print("#pragma GCC diagnostic pop\n");
   buffer->print("#endif\n\n");
 }
 
